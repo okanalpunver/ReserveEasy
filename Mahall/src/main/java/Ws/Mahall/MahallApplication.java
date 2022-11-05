08b4dd3b4de1e804ba874cb04aa49eee821d5386
okan.unver <okan.unver@obss.tech>
2022-11-06 01:59:54 +0300

    Initial commit

---
+++ Mahall/src/main/java/Ws/Mahall/MahallApplication.java
@@ -0,0 +1,13 @@
+package Ws.Mahall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahallApplication.class, args);
+	}
+
+}
---
+++ Mahall/src/test/java/Ws/Mahall/MahallApplicationTests.java
@@ -0,0 +1,13 @@
+package Ws.Mahall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
