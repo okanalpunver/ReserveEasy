6ac9ce16e4474ff529484b234ec01f2f13da7057
Okan Alp Ünver <okanalpunver@gmail.com>
2022-11-07 02:06:49 +0300

    name refactor

--- ReserveEasy/src/main/java/Ws/ReserveEasy/ReserveEasyApplication.java
+++ ReserveEasy/src/main/java/Ws/ReserveEasy/ReserveEasyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MahallApplication {
+public class ReserveEasyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MahallApplication.class, args);
+		SpringApplication.run(ReserveEasyApplication.class, args);
 	}
 
 }
