c718320219cb0e2ed541f565a1cd3e892d2fe7c5
okanalpunver <okanalpunver@gmail.com>
2022-11-09 01:30:16 +0300

    Initial commit

---
+++ ReserveEasyWS/src/main/java/ReserveEasy/ReserveEasyWS/ReserveEasyWsApplication.java
@@ -0,0 +1,13 @@
+package ReserveEasy.ReserveEasyWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveEasyWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveEasyWsApplication.class, args);
+	}
+
+}
---
+++ ReserveEasyWS/src/test/java/ReserveEasy/ReserveEasyWS/ReserveEasyWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ReserveEasy.ReserveEasyWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveEasyWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
