bc10daaa8ad9609e12954131e24649ba9e3e1293
osmanbhr99 <osman_bahar@hotmail.com>
2022-11-12 15:09:18 +0300

    Signin chart and ınter font

--- reserve-easy-fe/src/App.js
+++ reserve-easy-fe/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Button from "./Components/Button";
+import SignInPage from "./Components/SignInPage";
 function App() {
   return (
     <div className="App">
-      <Button/>
+        <head>
+            <style>@import url('https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap');</style>
+        </head>
+        <SignInPage/>
     </div>
   );
 }
