7a0eb8e32032ddfcf9fad5e4496c413ba2f89f5a
ykb1912 <111898602+ykb1912@users.noreply.github.com>
2022-11-13 15:29:46 +0300

    refactor navbar(logo added)

--- reserve-easy-fe/src/Components/Navbar.js
+++ reserve-easy-fe/src/Components/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Styles from "./Navbar.css"
 
-
 const Navbar = () =>{
     return(
         <div className={"top-nav"}>
+            <a className={"brand-logo"} href={"#"}>
+                <div className={"logo-image"}>
+                    <img src={"/logo.png"}/>
+                </div>
+            </a>
             <a className={"active"} href={"#"}>Anasayfa</a>
-            <a href={"#"}>Randevular</a>
-            <a href={"#"}>Arama</a>
+            <a href={"#"}>Kategoriler</a>
+            <a href={"#"}>En Yüksek Puan Alanlar</a>
+            <a href={"#"}>Size Özel</a>
+            <input type={"text"} placeholder={"Arama"}/>
+            <div className={"login-nav"}>
+                <a href={"#"}>Giriş Yap</a>
+            </div>
         </div>
     )
 }
