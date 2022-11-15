9b2999afab829b9132085c032ced3abd9e52c751
osmanbhr99 <osman_bahar@hotmail.com>
2022-11-15 14:42:47 +0300

    navbar.js and navbar.css update

--- reserve-easy-fe/src/Components/Navbar.js
+++ reserve-easy-fe/src/Components/Navbar.js
@@ -4,20 +4,28 @@ import Styles from "./Navbar.css"
 const Navbar = () =>{
     return(
         <div className={"top-nav"}>
-            <a className={"brand-logo"} href={"#"}>
-                <div className={"logo-image"}>
-                    <img src={"/logo.png"}/>
+            <div className="navbar">
+                <div className="logo">
+                    <a className={"brand-logo"} href={"#"}>
+                        <div className={"logo-image"}>
+                            <img src={"/logo.png"}/>
+                        </div>
+                    </a>
                 </div>
-            </a>
-            <a className={"active"} href={"#"}>Anasayfa</a>
-            <a href={"#"}>Kategoriler</a>
-            <a href={"#"}>En Yüksek Puan Alanlar</a>
-            <a href={"#"}>Size Özel</a>
-            <div className={"nav-searchbar"}>
-                <input type={"text"} placeholder={"Arama"}/>
-            </div>
-            <div className={"login-nav"}>
-                <a href={"#"}>Giriş Yap</a>
+                <div className="search">
+                    <div className={"nav-searchbar"}>
+                        <input className="search-bar" type={"text"} placeholder={"Arama"}/>
+                    </div>
+                    <div className="nav-menu">
+                    <a className={"active"} href={"#"}>Anasayfa</a>
+                    <a href={"#"}>Kategoriler</a>
+                    <a href={"#"}>En Yüksek Puan Alanlar</a>
+                    <a href={"#"}>Size Özel</a>
+                    </div>
+                </div>
+                <div className={"login-nav"}>
+                     <a href={"#"}>Giriş Yap</a>
+                </div>
             </div>
         </div>
     )
